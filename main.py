69f5ee98678d360f2772df25a1f0e7f5f03fa816
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-01 11:34:21 -0800

    don't know what's going on. It worked just a second ago, but now its saying that ODrive_Axis doesn't exist!!???

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import time
 from conference_sand_table_class import ConferenceSandTable
 
 table = ConferenceSandTable()
-table.draw_equation("", 1)
+# table.draw_equation("", 1)
 # time.sleep(5)
 # table.stop_theta()
+
+direction = "in"
+while True:
+    table.move(direction)
+    direction = "in" if direction == "out" else "out"
+    time.sleep(1.5)
+
+
