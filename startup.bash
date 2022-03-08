2c8189245125c99986a2d4cda13e6224e999e348
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-08 09:15:56 -0800

    added ssh

--- startup.bash
+++ startup.bash
@@ -1,5 +1,7 @@
-# SSH to the pi
 # shellcheck disable=SC2164
+
+# SSH to the pi
+# ssh pi@conference-sand-table-v2-pi.local
 cd ~/projects/ConferenceSandTable/Server
 export FLASK_APP=server
 flask run --host=0.0.0.0
