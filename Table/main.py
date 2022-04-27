5494b591a6cb9e04dba20332d5307ad3d60d343d
ZSchweyk <zeynschweyk@dpengineering.org>
2022-04-27 14:17:45 -0700

    minor changes for tomorrow!

--- Table/main.py
+++ Table/main.py
@@ -10,7 +10,7 @@ table.home()
 
 # table.emergency_stop()
 
-info = table.draw_equation("sin(6 * theta)", 2 * pi, theta_speed=.25, scale_factor=1, sleep=.005)
+info = table.draw_equation("sin(6 * theta)", 2 * pi, theta_speed=.8, scale_factor=1, sleep=.005)
 
 print("info:", info)
 
