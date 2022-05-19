a3540d1fd2fbf0b75c2635916648ecebb49dcfcf
ZSchweyk <zeynschweyk@dpengineering.org>
2022-05-19 11:00:07 -0700

    modified copying directory commands

--- copy_ClientPi_commands.sh
+++ copy_ClientPi_commands.sh
@@ -1,1 +1,1 @@
-scp -r /home/soft-dev/Documents/Projects/ConferenceSandTable/ClientPi pi@conference-sand-table-v2-radius-pi:projects/ConferenceSandTable/
+scp -r /home/soft-dev/Documents/Projects/ConferenceSandTable/ClientPi pi@conference-sand-table-v2-radius-pi:/home/pi/projects/ConferenceSandTable
---
+++ copy_Webserver_commands.sh
@@ -0,0 +1,1 @@
+scp -r /home/soft-dev/Documents/Projects/ConferenceSandTable/Webserver pi@conference-sand-table-v2-pi:/home/pi/projects/ConferenceSandTable
