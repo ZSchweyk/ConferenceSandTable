e727fe47300932f33c81b7f8ac2d1c5664562965
ZSchweyk <zeynschweyk@dpengineering.org>
2022-05-20 08:36:35 -0700

    updated webserver commands to copy

--- copy_Webserver_commands.sh
+++ copy_Webserver_commands.sh
@@ -1,5 +1,1 @@
 scp -r /home/soft-dev/Documents/Projects/ConferenceSandTable/Webserver pi@conference-sand-table-v2-pi:/home/pi/projects/ConferenceSandTable
-
-
-
-scp -r /home/soft-dev/Documents/Projects/ConferenceSandTable/Webserver pi@conference-sand-table-v2-pi:/var/www/FLASKAPPS/ConferenceSandTable
